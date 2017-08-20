d0c29ff6817c9207dbc02d128c693ca958c0481c
Surendra Shukla <surenshukla8@gmail.com>
2017-08-20 19:56:21 +0530

    Unecessary files deletion

--- adapter/example1/paymentInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Simple Interface for each Adapter we create
-interface paymentInterface
-{
-    public function pay($amount);
-}
--- adapter/example2/AdapterInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface AdapterInterface {
-    public function send($params);
-    public function read($params);
-}
